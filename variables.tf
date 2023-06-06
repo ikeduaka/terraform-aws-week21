0d1080fc8c8008a2c4c5177c28a82067cdea043f
Ike Duaka <ikeduaka@yahoo.com>
2023-06-06 05:25:37 +0000

    week_21-01

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "ami_id" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-053b0d53c279acc90"
+  }
+}
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+  }
+}
